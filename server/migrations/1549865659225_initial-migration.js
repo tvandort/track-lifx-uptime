f35c8922bfb5a41669023b31e35ff503bc763dc9
Tom <tvandort@gmail.com>
2019-02-11 03:22:47 -0500

    WIP

---
+++ server/migrations/1549865659225_initial-migration.js
@@ -0,0 +1,3 @@
+exports.up = pgm => {
+  pgm.createTable("example");
+};
--- server/scripts/start.sh
+++ server/scripts/start.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 
 tsc
+echo aaaaaaaaaaaaaaaahhhajskdkaj
+echo $DATABASE_URL
+yarn migrate up
 node $1 build/index.js
