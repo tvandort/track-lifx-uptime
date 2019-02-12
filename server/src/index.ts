f0b9d91b1f8d513a419d2b895d8abbe5309fbeda
Tom <tvandort@gmail.com>
2019-02-12 00:54:57 -0500

    Remove verbosity in output.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,3 @@
-// import LifxLan from "node-lifx-lan";
-// console.log("Booting.");
-
-// // setInterval(() => {
-// //   console.log("Listening");
-// // }, 1000);
-
-// const discover = async () => {
-//   const list = await LifxLan.discover();
-//   console.log(list);
-//   setTimeout(discover, 1000);
-// };
-
-// discover();
-
-// // LifxLan.discover().then(list => console.log(list));
-
 import { Client } from "node-lifx";
 
 const client = new Client();
@@ -24,6 +7,5 @@ client.on("light-new", light => {
 });
 
 client.init({
-  debug: true,
   broadcast: "192.168.1.255"
 });
