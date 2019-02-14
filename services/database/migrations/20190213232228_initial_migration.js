71cd700c50a75dc2fde9dc7245c76e44a9270425
Tom <tvandort1@gmail.com>
2019-02-14 02:08:42 -0500

    Initial migration.

---
+++ services/database/migrations/20190213232228_initial_migration.js
@@ -0,0 +1,8 @@
+// @ts-check
+var knex = require("knex");
+
+/** @type {function(knex, Promise): void} */
+exports.up = async function(knex, Promise) {};
+
+/** @type {function(knex, Promise): void} */
+exports.down = function(knex, Promise) {};
