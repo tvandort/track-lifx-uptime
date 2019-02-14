9a6f1edf8e4ab0de9bd579bb22c7fdb9412c00a6
Tom <tvandort1@gmail.com>
2019-02-14 02:08:30 -0500

    Define migration process.

---
+++ services/database/index.js
@@ -0,0 +1,29 @@
+// @ts-check
+
+const waitOn = require("wait-on");
+
+const config = require("./knexfile");
+const knex = require("knex");
+
+const databasePort = `tcp:${process.env.DB_ADDRESS}:${process.env.DB_PORT}`;
+
+(async () => {
+  try {
+    const resources = [databasePort];
+    await waitOn({
+      resources
+    });
+  } catch (error) {
+    console.log("Error waiting for database to start up: ", error);
+    process.exit(1);
+  }
+
+  try {
+    const client = knex(config);
+    await client.migrate.latest();
+    process.exit(0);
+  } catch (error) {
+    console.log("Error in migration: ", error);
+    process.exit(1);
+  }
+})();
