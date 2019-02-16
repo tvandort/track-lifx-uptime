0a632b9f126a32dc4b20e16fb32903b7216ea531
Tom <tvandort@gmail.com>
2019-02-15 23:29:21 -0500

    Preserve local node modules and install on docker.

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 
-yarn clean --yes
 docker-compose build
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml up $1
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 
-yarn clean --yes
 docker-compose build
 docker-compose up $1
