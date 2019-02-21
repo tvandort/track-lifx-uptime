09c4c2c51a060f5120e1642ecc785045edc15094
Tom <tvandort1@gmail.com>
2019-02-21 03:02:26 -0500

    Quick sql file generator.

---
+++ services/migrations/src/commands/make-migration.ts
@@ -0,0 +1,53 @@
+import fs, { write } from "fs";
+import path from "path";
+
+const reportFileCreationError = (error: Error) => {
+  if (error) {
+    console.log(error);
+  }
+};
+
+const writeOptions = {
+  flag: "w"
+};
+
+const getTimestampGivenDate = (targetDate: Date) => {
+  const year = targetDate.getFullYear();
+  const month = targetDate.getUTCMonth() + 1;
+  const day = targetDate.getUTCDate();
+  const milliseconds = targetDate.getUTCMilliseconds();
+  return `${year}${month}${day}${milliseconds}`;
+};
+
+const writeDownMigration = (name: string) => {
+  fs.writeFile(
+    `./src/migrations/${name}.down.sql`,
+    "",
+    writeOptions,
+    reportFileCreationError
+  );
+};
+
+const writeUpMigration = (name: string) => {
+  fs.writeFile(
+    `./src/migrations/${name}.up.sql`,
+    "",
+    writeOptions,
+    reportFileCreationError
+  );
+};
+
+const createMigrations = (name: string) => {
+  const timestamp = getTimestampGivenDate(new Date());
+  const fileName = `${timestamp}_${name}`;
+  writeUpMigration(fileName);
+  writeDownMigration(fileName);
+};
+
+const [, , name] = process.argv;
+if (name) {
+  createMigrations(name);
+} else {
+  console.log("not name");
+  process.exit(1);
+}
